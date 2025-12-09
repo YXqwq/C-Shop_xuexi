589e9cfa9ba731f984aa3f2e68c9a549be7bdefd
YXqwq <3140402871@qq.com>
2025-12-09 23:11:16 +0800

    每日学习

---
+++ 2025/变量知识点练习 12.9/变量知识点练习 12.9/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace 变量知识点练习_12._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 36.6;
+            Console.WriteLine(num);//结果为 36.6
+            //名字
+            string mingzi = "qwq";
+            //年龄
+            int nianling = 18;
+            //性别
+            bool six = false;
+            //身高
+            int shengao = 170;
+            //体重
+            int tizhong = 105;
+            //家庭住址
+            string dizi = "命运石之门";
+            //小明成绩
+            int shuxue = 80;
+            int yuwen = 70;
+            int yingyu = 90;
+            Console.WriteLine("小明的数学成绩为"+shuxue);
+            Console.WriteLine("小明的语文成绩为" + yuwen);
+            Console.WriteLine("小明的数学成绩为" + yingyu);
+
+        }
+    }
+}
